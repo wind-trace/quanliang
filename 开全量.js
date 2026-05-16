5241cc03f478331c795b9eee15f03f12c8b134c4
459521475 <132978466+459521475@users.noreply.github.com>
2026-05-16 13:51:12 +0800

    Update 开全量.js

--- 开全量.js
+++ 开全量.js
@@ -1,3 +1,6 @@
+NAPCAT_HTTP_HOST = '127.0.0.1' // napcat开启的http服务器host
+NAPCAT_HTTP_PORT = 3000 // napcat开启的http服务器端口
+NAPCAT_AUTH_TOKEN = 'napcat_uin_to_uid' // napcat鉴权token
 export class quanliang extends plugin {
     constructor() {
         super({
@@ -31,7 +34,7 @@ export class quanliang extends plugin {
             let url = `https://club.vip.qq.com/transfer?open_kuikly_info=${encodeURIComponent(JSON.stringify(info))}`
             e.reply('默认使用本群，如果需要指定群则艾特对应官机+开全量+群号\n'+url)
         } else if(this.e.adapter_id === 'QQBot'){
-            //if(e.raw.t.==='GROUP_MESSAGE_CREATE') return this.reply('本群已经开启了全量主动消息')
+            //if(e.raw.t.==='GROUP_MESSAGE_CREATE') return this.reply('')
             let group_id = Number(this.e.msg.replace(/#?开全量/,'').trim())
             if (!group_id) return this.reply(['请发送群号\r示例：开全量123456789\r若群号输入错误则无法成功授权\r![img #1080px #888px](https://cnb.cool/windtrace/wind-img/-/git/raw/main/%E5%85%A8%E9%87%8F%E4%B8%BB%E5%8A%A8.jpeg)',segment.button([{text:'开全量',input:'开全量'}])])
             for (const i of Bot.uin){
@@ -51,7 +54,27 @@ export class quanliang extends plugin {
                     return this.reply(['请群主点击下放按钮授权\r#需要更新QQ到最新版本(9.2.90正式版以上)\r若群号输入错误则无法成功授权\r![img #1080px #888px](https://cnb.cool/windtrace/wind-img/-/git/raw/main/%E5%85%A8%E9%87%8F%E4%B8%BB%E5%8A%A8.jpeg)',segment.button([{text:'请群主点击此按钮授权',link:url}])])
                 }
             }
-            return false
+            let user = await fetch(`http://${NAPCAT_HTTP_HOST}:${NAPCAT_HTTP_PORT}/get_stranger_info`,{
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${NAPCAT_AUTH_TOKEN}`
+                },
+                body: JSON.stringify({
+                user_id: e.self_id
+                })
+            })
+            user = await user.json()
+            let uid = user.data?.uid
+            const info = {
+                page_name: 'ai_group_service_agreement_pop_page',
+                groupCode: Number(group_id),
+                botUin: Number(e.self_id),
+                botUid: uid,
+                screen: 1
+            }
+            let url = `https://club.vip.qq.com/transfer?open_kuikly_info=${encodeURIComponent(JSON.stringify(info))}`
+            return this.reply(['请群主点击下放按钮授权\r#需要更新QQ到最新版本(9.2.90正式版以上)\r若群号输入错误则无法成功授权\r![img #1080px #888px](https://cnb.cool/windtrace/wind-img/-/git/raw/main/%E5%85%A8%E9%87%8F%E4%B8%BB%E5%8A%A8.jpeg)',segment.button([{text:'请群主点击此按钮授权',link:url}])])   
         }
         return false
     }
